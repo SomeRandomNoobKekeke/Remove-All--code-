ca344d4a485089ad7637dcccfe48795fef8d0c58
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-04-12 07:22:35 +0300

    sync SubmarineSettings with default

--- CSharp/Client/Settings/SubmarineSettings.cs
+++ CSharp/Client/Settings/SubmarineSettings.cs
@@ -10,8 +10,8 @@ namespace RemoveAll
   public class SubmarineSettings : IConfig
   {
     public float CullInterval { get; set; } = 0.25f;
-    public int CullMarginX { get; set; } = 500;
-    public int CullMarginY { get; set; } = 500;
+    public int CullMarginX { get; set; } = 0;
+    public int CullMarginY { get; set; } = 0;
     public float CullMoveThreshold { get; set; } = 50f;
   }
 
