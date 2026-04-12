854d6c045a876e3e09cfde837bfbbc5ad7851843
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-04-12 08:26:20 +0300

    delete all before rebase

--- ContentParser/settings/components/main.js
+++
@@ -1,9 +0,0 @@
-window.component.Main = function () {
-  let main = ce({ name: 'main', innerHTML: 'main' })
-  main.set = (content) => {
-    main.innerHTML = ''
-    main.append(content)
-  }
-
-  return main
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-Source code for [this mod](https://steamcommunity.com/sharedfiles/filedetails/?id=3043340865)
-
-You can just put it in LocalMods and it'll work
